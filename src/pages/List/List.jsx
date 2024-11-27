d87a85c345d66e3ac212439cc6ba34a99fd1d9d3
Md-Asraful-Islam-Subbir <asraful17@cse.pstu.ac.bd>
2024-11-27 21:30:24 +0600

    fail

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,60 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import axios from "axios"
-import { toast } from 'react-toastify'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "./List.css";
 
-const List = ({url}) => {
-  
-  const [list,setList]=useState([])
-  const fetchList=async()=>{
-    const response=await axios.get(`${url}/api/food/list`);
-    if(response.data.data){
-      setList(response.data.data);
+const List = ({ url }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      setList(response.data.data || []);
+    } catch (error) {
+      console.error("Error fetching food list:", error);
+      toast.error("Unable to fetch food list");
     }
-    else{
-      toast.error("Error");
+  };
+
+  const removeFood = async (id) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, { id });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        fetchList();
+      } else {
+        toast.error("Failed to remove food");
+      }
+    } catch (error) {
+      console.error("Error removing food:", error);
+      toast.error("Error removing food");
     }
-  }
-  const removeFood=async(foodId)=>{
-    const response=await axios.post(`${url}/api/food/remove`,{id:foodId});
-    await fetchList();
-    if(response.data.success){
-      toast.success(response.data.message);
-    }
-    else{
-      toast.error("Error");
-    }
-  }
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     fetchList();
-  },[])
+  }, []);
+
   return (
-    <div className='list add flex-col'>
-<p>All Foods List</p>
-<div className="list-table">
-  <div className="list-table-format title">
-  <b>Image</b>
-  <b>Name</b>
-  <b>Category</b>
-  <b>Price</b>
-  <b>Action</b>
-  </div>
-</div>
-{list.map((item,index)=>{
-  return(
-    <div key={index} className="list-table-format">
-      <img src={`${url}/images/`+item.image} alt="" />
-      <p>{item.name}</p>
-      <p>{item.category}</p>
-      <p>${item.price}</p>
-      <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
+    <div className="list">
+      <h3>Food List</h3>
+      {list.map((item) => (
+        <div key={item._id} className="list-item">
+          <img src={`${url}/images/${item.image}`} alt={item.name} />
+          <p>{item.name}</p>
+          <p>{item.category}</p>
+          <p>${item.price}</p>
+          <button onClick={() => removeFood(item._id)}>Remove</button>
+        </div>
+      ))}
     </div>
-  )
-})
+  );
+};
 
-}
-    </div>
-  )
-}
-
-export default List
+export default List;
